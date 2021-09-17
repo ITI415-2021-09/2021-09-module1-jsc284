91524f00d4ded5897d4ae38704334af33d46191c
jsc284 <jsc284@scarletmail.rutgers.edu>
2021-09-17 00:09:44 -0400

    Edit 7: Basic Movement/Changing Direction/Changing Direction Randomly
    Edited script "AppleTree" to account for Basic Movement section from text.
    Set Speed of script of object "AppleTree" to 10 under Inspector.
    Edited script "AppleTree" to account for Changing Direction section from text.
    Edited script "AppleTree" to account for Changing Direction Randomly section from text.
    Set Chance to Change Direction of script of object "AppleTree" to 0.08 under Inspector.

--- Assets/01-Apple Picker/Prefabs/AppleTree.cs
+++ Assets/01-Apple Picker/Prefabs/AppleTree.cs
@@ -29,6 +29,26 @@ public class AppleTree : MonoBehaviour
     void Update()
     {
         // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
         // Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Move right
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Move left
+        } 
+    }
+
+    void FixedUpdate ()
+    {
+        // Changing Direction Randomly is now time-based because of FixedUpdate ()
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; // Change direction
+        }
     }
 }
