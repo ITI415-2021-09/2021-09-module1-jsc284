9e39ae8108a0d067fece7e3a0a67a372b5f9d0ae
jsc284 <jsc284@scarletmail.rutgers.edu>
2021-09-17 00:47:57 -0400

    Edit 8: Dropping Apples/Physics Layers/Stopping Apples
    Set ApplePrefab to prefab "Apple" for AppleTree script of object "AppleTree" under Inspector.
    Edited script "AppleTree" to account for Dropping Apples section from text.
    Set IsKinematic for Rigidbody component of object "AppleTree" under Inspector.
    Added Layers 8 renamed "AppleTree", 9 renamed "Apple", 10 renamed "Basket" to Layers of object "AppleTree" under Inspector.
    Adjusted Layer Collision Matrix so that "Apple" does not collide with "AppleTree" or "Apple".
    Set layers of prefabs "Apple" to layer "Apple", "Basket" to "Basket", and "AppleTree" to "AppleTree".
    Created script renamed "Apple" in "Prefabs" folder.
    Edited script "Apple" to take account for Stopping Apples section from text.
    Attached script "Apple" to prefab "Apple" under Inspector.

--- Assets/01-Apple Picker/Prefabs/AppleTree.cs
+++ Assets/01-Apple Picker/Prefabs/AppleTree.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour
-{
+{ 
     // Prefab for instantiating apples
     public GameObject applePrefab;
 
@@ -23,6 +23,14 @@ public class AppleTree : MonoBehaviour
     void Start()
     {
         // Dropping apples every second
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple ()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
     }
 
     // Update is called once per frame
