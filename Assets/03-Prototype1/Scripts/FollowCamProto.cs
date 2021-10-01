fb543b1b4f89765478a6feec11cac48cff58070d
jsc284 <jsc284@scarletmail.rutgers.edu>
2021-10-01 02:04:52 -0400

    Edit 4: FollowCamera from MissionDemolition
    Bits and scraps from pages 506, 511, 512, 514, 515, & 516. In conclusion, no idea what happened as a result.
    Renamed prefab "Projectile" as "ProjectileA". Created prefab "Projectile".

---
+++ Assets/03-Prototype1/Scripts/FollowCamProto.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FollowCamProto : MonoBehaviour
+{
+    public GameObject prefabProjectile;
+
+    static public GameObject POI; // The static point of interest
+
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
+    [Header("Set Dynamically")]
+    public float camZ; // The desired Z pos of the camera
+    public GameObject projectile;
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        if (POI == null) return;
+
+        // Get the position of the poi
+        Vector3 destination = POI.transform.position;
+        // Limit the X & Y to minimum values
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        // Force destination.z to be camZ to keep the camera far enough away
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.position = destination;
+        // Set the orthographicSize of the Camera to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowCamProto.POI = projectile;
+    }
+}
